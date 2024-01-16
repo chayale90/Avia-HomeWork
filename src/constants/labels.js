8e087b7158166e7b15efd3dce590ebb32cf4871b
Chaya Segal <chayale90@gmail.com>
2024-01-17 00:32:17 +0200

    labels and routes constants

---
+++ src/constants/labels.js
@@ -0,0 +1,20 @@
+export const HEADLINES = {
+  USERSLIST: "רשימת לקוחות",
+  ADDUSER: "הוספת לקוח",
+};
+
+export const USER_PROPERTIES = {
+  FIRSTNAME: "שם פרטי",
+  LASTNAME: "שם משפחה",
+  PHONE: "טלפון",
+  EMAIL: "מייל",
+  PLAN: "תכנית",
+};
+
+export const PLANS = [
+  { id: 1, name: "care" },
+  { id: 2, name: "light" },
+  { id: 3, name: "connect" },
+  { id: 4, name: "home" },
+  { id: 5, name: "navigate" },
+];
