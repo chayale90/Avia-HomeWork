6ebdd28606724d08ec3d73e4f359553af7806a73
Chaya Segal <chayale90@gmail.com>
2024-01-17 13:17:33 +0200

    constants headline

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,12 +1,13 @@
 import { Container, Typography } from '@mui/material'
 import UsersList from '../components/userList/UsersList'
+import { HEADLINES } from '@/constants/labels';
 
 export default function UsersPage() {
     return (
         <Container maxWidth="lg" sx={{ paddingY: 4 }}>
 
             <Typography variant="h3" component="h1" mb={4} textAlign={"center"}>
-                רשימת לקוחות
+            {HEADLINES.USERSLIST}
             </Typography>
             <UsersList />
 
